87e02a9c7ae9d600ded4181d02a1c71ea6d9cd87
MasiaPoli <126983383+MasiaPoli@users.noreply.github.com>
2024-12-05 18:36:18 +0300

    Add files via upload

--- lst.h
+++ lst.h
@@ -37,15 +37,15 @@ template <class T> class lst
 public:
     virtual  void push(T a)=0;
     virtual T top() =0;
-    virtual  element<T>* get_top()=0;
+    virtual  const T& get_top()const =0;
     virtual void pop() =0;
     virtual   bool isEmpty() =0;
-    virtual  void print() =0;
-    virtual void skan()=0;
     virtual  void delall() =0;
     virtual size_t lendgh()=0;
-    friend  std::ostream& operator << (std::ostream &stream, lst<T> &t);
-    friend  std::istream& operator >> (std::istream &stream, lst<T> &t);
+    template <class U> friend std::ostream& operator << (std::ostream &stream, lst<U> &t);
+    template <class U> friend std::istream& operator >> (std::istream &stream, lst<U> &t);
+    virtual  void print() =0;
+    virtual void skan()=0;
 };
 template <class T>  std::ostream& operator << (std::ostream &stream, lst<T> &t)
 {
@@ -66,84 +66,34 @@ public:
         ld=0;
     }
     steck(steck& s);
-    steck(steck&& s)
-    {
-        head=nullptr;
-        ld=s.ld;
-        head=s.head;
-        s.head=nullptr;
-    }
-    ~steck ()
-    {
-        while(head)
-        {
-            element<T>* h2=head;
-            head=head->prev;
-            delete h2;
-        }
-    }
+    steck(steck&& s);
+    ~steck ();
     void push(T a)
     {
         element<T>* h=new element<T>(a, head);
         head=h;
         ld++;
     }
-    void skan()
-    {
-        size_t n;
-        std::cin>>n;
-        for(size_t i=0; i<n; i++)
-        {
-            T x;
-            std::cin>>x;
-            push(x);
-        }
-    }
+    void skan();
     T top()
     {
         return head->a;
     }
-    element<T>* get_top()
+    const T& get_top() const
     {
-        return head;
+        return head->a;
     }
-    void pop()
-    {
-        if(head)
-        {
-            element<T>* h2=head;
-            head=head->prev;
-            delete h2;
-            ld--;
-        }
-    }
+    void pop();
     bool isEmpty()
     {
         return !head;
     }
-    void delall()
-    {
-        ld=0;
-        while(head)
-        {
-            element<T>* h2=head;
-            head=head->prev;
-            delete h2;
-        }
-    }
+    void delall();
     size_t lendgh()
     {
         return ld;
     }
-    void print()
-    {
-        element<T>* e=head;
-        while(e)
-        {
-            std::cout<<e->a<<" ";
-            e=e->prev;
-        }
-    }
+    void print();
     steck<T>& operator = (const steck<T>& s);
 private:
     element <T>* head;
@@ -166,6 +116,22 @@ template <class T> steck<T> :: steck ( steck<T>& s)
 
     }
 }
+template <class T> steck<T> ::  steck(steck&& s)
+{
+    head=nullptr;
+    ld=s.ld;
+    head=s.head;
+    s.head=nullptr;
+}
+template <class T> steck<T> ::  ~steck ()
+    {
+        while(head)
+        {
+            element<T>* h2=head;
+            head=head->prev;
+            delete h2;
+        }
+    }
 template <class T> steck<T>&  steck<T>:: operator = (const steck<T>& s)
 {
     delall();
@@ -185,13 +151,48 @@ template <class T> steck<T>&  steck<T>:: operator = (const steck<T>& s)
     }
     return *this;
 }
-
+template <class T> void steck<T>:: pop()
+{
+    if(head)
+    {
+        element<T>* h2=head;
+        head=head->prev;
+        delete h2;
+        ld--;
+    }
+}
+template <class T> void steck<T>:: skan()
+{
+    size_t n;
+    std::cin>>n;
+    for(size_t i=0; i<n; i++)
+    {
+        T x;
+        std::cin>>x;
+        push(x);
+    }
+}
+template <class T> void steck<T>:: print()
+{
+    element<T>* e=head;
+    while(e)
+    {
+        std::cout<<e->a<<" ";
+        e=e->prev;
+    }
+}
+template <class T> void steck<T>:: delall()
+{
+    ld=0;
+    while(head)
+    {
+        element<T>* h2=head;
+        head=head->prev;
+        delete h2;
+    }
+}
 template <class T> class och: public lst<T>
 {
-private:
-    element <T>* head;
-    element <T>* tail;
-    size_t ld;
 public:
     och()
     {
@@ -199,100 +200,147 @@ public:
         tail=nullptr;
         ld=0;
     }
-    och(const och& q)
-    {
-        if(q.head)
-        {
-            ld=q.ld;
-            head=new element<T> (q.head->a);
-            element<T>* h=head;
-            element<T>* q_h=q.head;
-            while(q_h!=q.tail)
-            {
-                q_h=q_h->prev;
-                element<T>* h2=new element<T>(q_h->a);
-                tail=h2;
-                h->prev=h2;
-                h=h->prev;
-            }
-        }
-    }
-    och(och&& q)
-    {
-        head=q.head;
-        tail=q.tail;
-        ld=q.ld;
-        q.head=nullptr;
-        q.tail=nullptr;
-    }
+    och(const och& q);
+    och(och&& q);
 
-    ~och()
-    {
-        while(head)
-        {
-            element<T>* h2=head;
-            head=head->prev;
-            delete h2;
-        }
-    }
+    ~och();
 
-    void push(T a)
-    {
-        element<T>* h=new element<T>(a);
-        if(!head)
-        {
-            head=h;
-            tail=h;
-        }
-        else
-        {
-            if(tail==head)
-            {
-                head->prev=h;
-            }
-            tail->prev=h;
-            tail=h;
-        }
-        ld++;
-    }
-    void skan()
-    {
-        size_t n;
-        std::cin>>n;
-        for(size_t i=0; i<n; i++)
-        {
-            T x;
-            std::cin>>x;
-            push(x);
-        }
-    }
-    void pop()
-    {
-        if(!isEmpty())
-        {
-            element<T>* h2=head;
-            head=head->prev;
-            delete h2;
-            if(!tail)
-            {
-                head=nullptr;
-            }
-            ld--;
-        }
-    }
+    void push(T a);
+    void skan();
+    void pop();
     T top()
     {
         return head->a;
     }
-    element<T>* get_top()
+    const T& get_top() const
     {
-        return head;
+        return head->a;
     }
     bool isEmpty()
     {
         return !head;
     }
-    void delall()
+    void delall();
+    size_t lendgh()
+    {
+        return ld;
+    }
+    och& operator = (const och& q);
+    void print();
+private:
+    element <T>* head;
+    element <T>* tail;
+    size_t ld;
+};
+template <class T> och<T> ::och(const och<T>& q)
+{
+    if(q.head)
+    {
+        ld=q.ld;
+        head=new element<T> (q.head->a);
+        element<T>* h=head;
+        element<T>* q_h=q.head;
+        while(q_h!=q.tail)
+        {
+            q_h=q_h->prev;
+            element<T>* h2=new element<T>(q_h->a);
+            tail=h2;
+            h->prev=h2;
+            h=h->prev;
+        }
+    }
+}
+template <class T> och<T> :: och(och&& q)
+{
+    head=q.head;
+    tail=q.tail;
+    ld=q.ld;
+    q.head=nullptr;
+    q.tail=nullptr;
+}
+template <class T> och<T> ::  ~och()
+{
+    while(head)
+    {
+        element<T>* h2=head;
+        head=head->prev;
+        delete h2;
+    }
+}
+template <class T> och<T>& och<T>::operator = (const och<T>& q)
+{
+    delall();
+    if(q.head)
+    {
+        ld=q.ld;
+        head=new element<T> (q.head->a);
+        element<T>* h=head;
+        element<T>* q_h=q.head;
+        while(q_h!=q.tail)
+        {
+            q_h=q_h->prev;
+            element<T>* h2=new element<T>(q_h->a);
+            tail=h2;
+            h->prev=h2;
+            h=h->prev;
+        }
+    }
+    return *this;
+}
+template <class T> void och<T>:: pop()
+{
+    if(!isEmpty())
+    {
+        element<T>* h2=head;
+        head=head->prev;
+        delete h2;
+        if(!tail)
+        {
+            head=nullptr;
+        }
+        ld--;
+    }
+}
+template <class T> void och<T>:: skan()
+{
+    size_t n;
+    std::cin>>n;
+    for(size_t i=0; i<n; i++)
+    {
+        T x;
+        std::cin>>x;
+        push(x);
+    }
+}
+template <class T> void och<T>:: push(T a)
+{
+    element<T>* h=new element<T>(a);
+    if(!head)
+    {
+        head=h;
+        tail=h;
+    }
+    else
+    {
+        if(tail==head)
+        {
+            head->prev=h;
+        }
+        tail->prev=h;
+        tail=h;
+    }
+    ld++;
+}
+template <class T> void och<T>:: print()
+{
+    element<T>* e=head;
+    while(e)
+    {
+        std::cout<<e->a<<" ";
+        e=e->prev;
+    }
+}
+template <class T> void och<T>:: delall()
     {
         while(head)
         {
@@ -300,38 +348,4 @@ public:
             pop();
         }
     }
-    size_t lendgh()
-    {
-        return ld;
-    }
-    och& operator = (const och& q)
-    {
-        delall();
-        if(q.head)
-        {
-            ld=q.ld;
-            head=new element<T> (q.head->a);
-            element<T>* h=head;
-            element<T>* q_h=q.head;
-            while(q_h!=q.tail)
-            {
-                q_h=q_h->prev;
-                element<T>* h2=new element<T>(q_h->a);
-                tail=h2;
-                h->prev=h2;
-                h=h->prev;
-            }
-        }
-        return *this;
-    }
-    void print()
-    {
-        element<T>* e=head;
-        while(e)
-        {
-            std::cout<<e->a<<" ";
-            e=e->prev;
-        }
-    }
-};
 #endif
